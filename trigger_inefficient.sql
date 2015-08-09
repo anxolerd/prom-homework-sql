10ebf01faa084ea143b8e29700d52f31c36f74d8
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-08-09 14:32:59 +0300

    inefficient implementation of trigger

---
+++ trigger_inefficient.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION do_count_products() RETURNS trigger AS $$ 
+  BEGIN 
+    UPDATE company c SET products_num = (
+        SELECT count(*) FROM product p WHERE p.company_id = c.id
+    ); 
+    RETURN NULL; 
+  END; 
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER count_products 
+  AFTER INSERT OR DELETE OR UPDATE OF company_id 
+  ON product 
+  EXECUTE PROCEDURE do_count_products();
